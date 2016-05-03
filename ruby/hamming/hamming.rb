924fb60736e556c893daeacff35629c2e9f1a35b
Raoul DIFFOUO <diraulo@gmail.com>
2016-05-03 22:38:00 +0200

    another go at the hamming exercise

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,7 +2,13 @@ class Hamming
   VERSION = 1
 
   def self.compute(strand_1, strand_2)
-    raise ArgumentError unless strand_1.length == strand_2.length
-    strand_1.chars.each_index.count { |i| strand_1[i] != strand_2[i] }
+    strand_of_unequal_length unless strand_1.length == strand_2.length
+    (0...strand_1.length).count { |i| strand_1[i] != strand_2[i] }
   end
+
+  def self.strand_of_unequal_length
+    raise ArgumentError, 'Strands must be of same length'
+  end
+
+  private_class_method :strand_of_unequal_length
 end
