b141e0804e41dc662d5c08536feca2027bdc7de6
Raoul DIFFOUO <diraulo@gmail.com>
2020-06-19 23:39:41 +0200

    ruby: hexadecimal exercise second iteration

--- ruby/hexadecimal/hexadecimal.rb
+++ ruby/hexadecimal/hexadecimal.rb
@@ -1,34 +1,22 @@
 # frozen_string_literal: true
 
 class Hexadecimal
+  DIGITS = '0123456789abcdef'
+
   def initialize(input)
-    @invalid = input =~ /[^0123456789abcdef]/
-    @chars = input.chars
+    @input = input
     @n = input.length - 1
   end
 
   def to_decimal
-    return 0 if invalid
-
-    chars.each_with_index.sum { |c, idx| int(c) * 16**(n - idx) }
+    chars.each_with_index.sum { |c, idx| DIGITS.index(c) * 16**(n - idx) }
   end
 
   private
 
-  attr_reader :chars, :n, :invalid
+  attr_reader :input, :n
 
-  def int(char)
-    return Integer(char) unless char =~ /[a-f]/
-
-    mapping = {
-      'a': 10,
-      'b': 11,
-      'c': 12,
-      'd': 13,
-      'e': 14,
-      'f': 15
-    }
-
-    mapping[char.to_sym]
+  def chars
+    input =~ /[^0-9a-f]/ ? [] : input.chars
   end
 end
